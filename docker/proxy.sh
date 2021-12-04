71f7665834742c967c120040c943dac09a495eeb
Phelps <phelpsmemo@gmail.com>
2021-12-04 15:58:39 -0300

    add base proxy docker environment

---
+++ docker/proxy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Copyright (C) 2021 Storm Project.
+#
+# storm-reprozip is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+#
+# Proxing the reprozip experiment bundle
+#
+storm-reprozip run --bundle /opt/input/package.rpz
